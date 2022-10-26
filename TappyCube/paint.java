e7afa2a987cff65bd93d723b21f911c65d409786
JordanUkawoko <jordanukawoko1@gmail.com>
2022-10-26 08:11:47 +0200

    Java Game IST Class

---
+++ TappyCube/paint.java
@@ -0,0 +1,108 @@
+package tappycube;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+
+public void paint(Graphics g)
+	{
+		g.setColor(Color.blue);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+		g.setColor(Color.brown);
+		g.fillRect(0, HEIGHT - 120, WIDTH, 120);
+
+		g.setColor(Color.green);
+		g.fillRect(0, HEIGHT - 120, WIDTH, 20);
+
+		g.setColor(Color.purple);
+		g.fillRect(cube.x, cube.y, cube.width, cube.height);
+
+		for (Rectangle column : columns)
+		{
+			paintColumn(g, column);
+		}
+
+		g.setColor(Color.white);
+		g.setFont(new Font("Arial", 1, 100));
+
+		if (!started)
+		{
+			g.drawString("Tappy Cube - Click to Start!", 75, HEIGHT / 2 - 50);
+		}
+
+		if (gameOver)
+		{
+			g.drawString("You Lost!!", 100, HEIGHT / 2 - 50);
+		}
+
+		if (!gameOver && started)
+		{
+			g.drawString(String.valueOf(score), WIDTH / 2 - 25, 100);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		tappyCube = new TappyCube();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		jump();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		if (e.getKeyCode() == KeyEvent.VK_SPACE)
+		{
+			jump();
+		}
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+
+	}
